e6d36c14420cf1bf4c25673971c4fda32d5066aa
Pawan R <pawansencha@Pawans-MacBook-Pro.local>
2026-09-14 21:46:12 +0530

    Add sitemap and robots.txt for search indexing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
+
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  site: 'https://www.pawanr.com',
+  integrations: [
+    mdx(),
+    sitemap(),
+  ],
 });
